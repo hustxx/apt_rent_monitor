333c3ace2a995d9eda86b64e952bafb74bb745c2
hustxx <xxbest1989@gmail.com>
2015-09-19 20:05:02 -0700

    final version after find the apartment

--- monitor_code/monitor.py
+++ monitor_code/monitor.py
@@ -9,23 +9,28 @@ import re
 class apt_rent_monitor:
 	
 	def __init__(self):
-		aptName = ["Archerstone", "Alborada", "Mission Peaks", "Mission Peaks II"]
+		aptName = ["Archerstone", "Alborada", "Presidio", "Mission Peaks", "Mission Peaks II"]
 		aptUrl = ["http://www.equityapartments.com/california/san-francisco-bay-apartments/fremont/archstone-fremont-center-apartments.aspx",
 		          "http://www.equityapartments.com/california/san-francisco-bay-apartments/fremont/alborada-apartments.aspx",
+			  "https://www.on-site.com/web/online_app/choose_unit?goal=6&attr=5&property_id=144840",
 			  "https://www.essexapartmenthomes.com/api/get-available/sfo1132",
 			  "https://www.essexapartmenthomes.com/api/get-available/sfo1205"]
 #		pattern1 = re.compile('<div class="floorplan">.*?<h3>(.*?)</h3>.*?<p>from <b>(.*?)</b>(.*?)</p>', re.S)
 		pattern1 = re.compile('<div class="unit">.*?<h4>(.*?)</h4>.*?<p><b>\$(.*?)</b>.*?<p>available <b>(.*?)</b>', re.S)
-		pattern2 = re.compile('{"unit_id":.*?"unit_type_floor_plan_name":"(.*?)","make_ready_date":.*?,"rent":(.*?),".*?"beds":"1.0".*?"available_date":"(.*?)"', re.S)
+#		pattern2 = re.compile('<td class="addr_or_apt" title="(.*?)".*?<span class="internal_rent">(.*?)</span>.*?data-available-date="(.*?)".*?data-floorplan-description="1 bed', re.S)	
+		pattern2 = re.compile('<td class="addr_or_apt" title="(.*?)".*?<span class="internal_rent">(.*?)</span>.*?data-available-date="0(.*?)"', re.S)       
+		pattern3 = re.compile('{"unit_id":.*?"unit_type_floor_plan_name":"(.*?)","make_ready_date":.*?,"rent":(.*?),".*?"beds":"1.0".*?"available_date":"(.*?)"', re.S)
 		
-		self.moveInDate = "06-24-2015"
+		self.moveInDate = "08-15-2015"
 		self.aptInfo = []
 		self.availableRooms = []
 		for i in range(2):
 			self.aptInfo.append([aptName[i], aptUrl[i], pattern1])
-		for j in range(2,4):
+                for m in range(2,3):
+                        self.aptInfo.append([aptName[m], aptUrl[m], pattern2])
+		for j in range(3,5):
 			aptUrl[j] = aptUrl[j] + "/" + self.moveInDate
-			self.aptInfo.append([aptName[j], aptUrl[j], pattern2])
+			self.aptInfo.append([aptName[j], aptUrl[j], pattern3])
 
 	def getPage(self, aptURL):
 		try:
@@ -68,7 +73,9 @@ class apt_rent_monitor:
 				print room[0], room[1], room[2]		
 
 spider = apt_rent_monitor()
-#pageCode = spider.getPage(spider.aptInfo[2][1])
+pageCode = spider.getPage(spider.aptInfo[2][1])
+print pageCode
 #result = re.findall(spider.aptInfo[2][2], pageCode)
+#print spider.aptInfo[1][2]
 #print result
-spider.start()
+#spider.start()
